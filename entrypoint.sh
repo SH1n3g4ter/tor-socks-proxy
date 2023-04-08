8ba0a515ad56b81d03b68694f571a69e36239781
Florian <91justway@gmail.com>
2023-04-08 17:25:14 +0200

    entrypoint update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 if ! grep -q "HashedControlPassword" /etc/tor/torrc
 then
-    printf "\nHashedControlPassword " >> /etc/tor/torrc && tor --hash-password $TOR_CONTROL_PASSWD >> /etc/tor/torrc
+    printf "\nHashedControlPassword " >> /etc/tor/torrc && tor --hash-password "$TOR_CONTROL_PASSWD" >> /etc/tor/torrc
 fi
 /usr/bin/tor -f /etc/tor/torrc
